c295cabadab8dad8b821711c88d5dd7bf84517f6
Maxim Zavitaev <vskweb@gmail.com>
2018-12-07 03:29:28 +0300

    Add command for sending messages

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\SendMessages::class
     ];
 
     /**
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+         $schedule->command('messages:send')
+                  ->everyMinute();
     }
 
     /**
